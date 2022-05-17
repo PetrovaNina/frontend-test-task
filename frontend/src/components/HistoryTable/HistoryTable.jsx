37e18dce17a5a007cbdbacb65ca9c0cab1467137
Nina Petrova <Nina_Petrova@epam.com>
2022-05-17 16:19:22 +0300

    feat: create a key generator function to avoid 'react key errors'

--- frontend/src/components/HistoryTable/HistoryTable.jsx
+++ frontend/src/components/HistoryTable/HistoryTable.jsx
@@ -1,4 +1,5 @@
 import cn from "classnames";
+import { generateKey } from "../../utils/generateKey";
 import { Container } from "../Layout";
 import s from "./HistoryTable.module.scss";
 
@@ -15,16 +16,18 @@ export const HistoryTable = () => {
           </tr>
         </thead>
         <tbody>
-          {Array(50).fill('test').map((e, i) => (
-            <tr className={s.row}>
-              <td className={s.first}>
-                <p className={s.colored}>Observation</p>
-              </td>
-              <td className={s.second}>Random description</td>
-              <td className={s.pre_last}>982641-34</td>
-              <td className={s.last}>Mar 28, 2022</td>
-            </tr>
-          ))}
+          {Array(50)
+            .fill("test")
+            .map((e, i) => (
+              <tr className={s.row} key={generateKey(s.row + i)}>
+                <td className={s.first}>
+                  <p className={s.colored}>Observation</p>
+                </td>
+                <td className={s.second}>Random description</td>
+                <td className={s.pre_last}>982641-34</td>
+                <td className={s.last}>Mar 28, 2022</td>
+              </tr>
+            ))}
         </tbody>
       </table>
     </section>
